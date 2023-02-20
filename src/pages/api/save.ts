49080e87aadee3870ff4b8a35bd827a209922a0b
jigsaw <m@jgs.me>
2023-02-20 22:14:31 +0900

    async save

--- src/pages/api/save.ts
+++ src/pages/api/save.ts
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { execSync } from "child_process";
+import { execSync, exec } from "child_process";
 import { writeFileSync } from "fs";
 
 type Body = {
@@ -14,14 +14,16 @@ export default async function handler(
 ) {
   const body = JSON.parse(req.body) as Body;
   if (body.ft.match(/\d{14}/) && body.id.match(/([A-Z]|\-)+/)) {
-    const r = execSync(
-      `docker run -v "$(pwd)"/output:/output radigo rec -id=${body.id} -s=${body.ft}`
+    exec(
+      `docker run -v "$(pwd)"/output:/output radigo rec -id=${body.id} -s=${body.ft}`,
+      () => {
+        const filename = `${body.ft}-${body.id}`;
+        execSync(`mkdir -p output/${filename}`);
+        execSync(`mv output/${filename}.aac output/${filename}/a.aac`);
+        writeFileSync(`output/${filename}/meta.json`, req.body);
+        res.status(200).json({ ok: true });
+      }
     );
-    const filename = `${body.ft}-${body.id}`;
-    execSync(`mkdir -p output/${filename}`);
-    execSync(`mv output/${filename}.aac output/${filename}/a.aac`);
-    writeFileSync(`output/${filename}/meta.json`, req.body);
-    res.status(200).json({ ok: true });
   } else {
     res.status(401).json({ ok: false });
   }
