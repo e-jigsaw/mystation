792fd20e7037b1cb5e572a3d0ecd86703e326ea0
jigsaw <m@jgs.me>
2023-07-30 23:36:05 +0900

    send discord processing

--- src/app/api/save/route.ts
+++ src/app/api/save/route.ts
@@ -2,13 +2,16 @@ import { NextResponse } from "next/server";
 import { exec, execSync } from "child_process";
 import { client } from "lib/getClient";
 import { updateList } from "lib/updateList";
+import { postMessage } from "lib/postMessage";
 
 export async function POST(req: Request) {
   const body = await req.json();
+  postMessage(`rcv: ${body.title}`);
   if (body.ft.match(/\d{14}/) && body.id.match(/([A-Z]|\-)+/)) {
     exec(
       `docker run -v ./output:/output radigo rec -o=mp3 -id=${body.id} -s=${body.ft}`,
       async () => {
+        postMessage(`donwloaded: ${body.title}`);
         const filename = `${body.ft}-${body.id}`;
         const [meta, file] = await Promise.all([
           client.putObject(
@@ -22,10 +25,10 @@ export async function POST(req: Request) {
             `./output/${filename}.mp3`
           ),
         ]);
-        console.log(meta, file);
+        postMessage(`uploaded: meta:${meta.etag}/file:${file.etag}`);
         execSync(`rm -rf ./output/${filename}.mp3`);
         await updateList();
-        console.log("updated RSS");
+        postMessage(`RSS Updated!`);
       }
     );
     return NextResponse.json({ ok: true }, { status: 200 });
