5e8fc2f2ccfa6d9499d9766f43fe51957b56eafe
jigsaw <m@jgs.me>
2024-08-02 03:16:13 +0000

    clean

--- src/app/api/list/route.ts
+++ src/app/api/list/route.ts
@@ -1,10 +1,6 @@
 import { updateList } from "lib/updateList";
 import { NextResponse } from "next/server";
 
-export async function GET() {
-  // TODO
-}
-
 export async function PUT() {
   await updateList();
   return NextResponse.json({ ok: true });
