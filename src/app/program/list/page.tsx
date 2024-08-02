30abc0bde47b630283fabe46788ddd2ce0de8ec9
jigsaw <m@jgs.me>
2024-08-02 03:16:57 +0000

    clean

--- src/app/program/list/page.tsx
+++ src/app/program/list/page.tsx
@@ -2,6 +2,5 @@ import { db } from "db/db";
 
 export default async function Page() {
   const res = await db.query.programs.findMany({ with: { radio: true } });
-  console.log(res);
   return <div className="p-2">works!</div>;
 }
