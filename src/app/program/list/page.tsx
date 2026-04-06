f44ab8e03e62c491b7506ffef7589f46d58fca53
jigsaw <m@jgs.me>
2026-04-06 03:40:54 +0000

    save

--- src/app/program/list/page.tsx
+++ src/app/program/list/page.tsx
@@ -1,6 +1,26 @@
 import { db } from "db/db";
+import Link from "next/link";
 
 export default async function Page() {
-  const res = await db.query.programs.findMany({ with: { radio: true } });
-  return <div className="p-2">works!</div>;
+  const res = await db.query.programs.findMany({
+    with: { radio: true },
+    limit: 20,
+  });
+  return (
+    <div className="p-2">
+      {res.map((prog) => (
+        <div key={prog.id} className="flex gap-1">
+          <div>{prog.id}</div>
+          <div>
+            {prog.pubDate.getMonth() + 1}/{prog.pubDate.getDate()}
+          </div>
+          <div>
+            <Link href={`/radio/show/${prog.radio?.id}`}>
+              {prog.radio?.name}
+            </Link>{" "}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
