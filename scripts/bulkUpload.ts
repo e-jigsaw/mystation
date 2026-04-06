16bee8771b2fe683e4a63b60a501a0b6f0dea28a
jigsaw <m@jgs.me>
2026-04-06 12:28:20 +0900

    fix path

--- scripts/bulkUpload.ts
+++ scripts/bulkUpload.ts
@@ -1,7 +1,7 @@
 import { client } from "lib/getClient";
 
 const main = async () => {
-  const glob = new Bun.Glob("./tmp/**/meta.json");
+  const glob = new Bun.Glob("./output/**/meta.json");
   for await (const file of glob.scan(".")) {
     const [_, __, prefix] = file.split("/");
     await client.fPutObject(
@@ -12,7 +12,7 @@ const main = async () => {
     await client.fPutObject(
       process.env.MINIO_BUCKET!,
       `output/${prefix}/a.mp3`,
-      `./tmp/${prefix}/a.mp3`
+      `./output/${prefix}/a.mp3`
     );
   }
 };
